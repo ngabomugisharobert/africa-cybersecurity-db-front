e2b65d51e6202b7714ac5a3b0e7eeb5beedbeb0f
NGABO MUGISHA ROBERT <ngabo.mugisha.bro@gmail.com>
2022-10-31 09:22:19 +0200

    Update projects.jsx

--- components/parts/projects.jsx
+++ components/parts/projects.jsx
@@ -182,6 +182,15 @@ import { Textarea } from "@material-tailwind/react";
         return (
           <div className="mt-5 container">
             <div className="alert alert-info">There are no applications</div>
+
+            <button
+              onClick={this.handleOpen}
+              className="focus:ring-2 focus:ring-offset-2 focus:ring-indigo-600 mt-4 sm:mt-0 inline-flex items-start justify-start px-6 py-3 bg-indigo-700 hover:bg-indigo-600 focus:outline-none rounded"
+            >
+              <p className="text-sm font-medium leading-none text-white">
+                Add Project
+              </p>
+            </button>
           </div>
         );
 
